cabeb329ed9219878110cbf798a098cd4b842bf5
oussamaode <oussamaode@users.noreply.github.com>
2026-06-13 13:10:51 +0100

    VaultKey PWA - Gestor de contraseñas offline

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE = 'vaultkey-v1';
+const ASSETS = [
+  'miPassZ.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((res) => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => {
+        return new Response('Sin conexión', { status: 503 });
+      });
+    })
+  );
+});
